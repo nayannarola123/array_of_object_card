3c316bae3cfc2e63d5bfc91f8a121420f0f78304
nayannarola123 <nayannarola123@gmail.com>
2024-06-03 12:08:27 +0530

    card

--- js/script.js
+++ js/script.js
@@ -1,272 +1,185 @@
-const myHonda = {
-    color: "red",
-    wheels: 2,
-    engine: {
-      cylinders: 4,
-      size: 2.2,
-    },
-}
-document.getElementById("color1").innerHTML=(`Color is ${myHonda.color}`);
-document.getElementById("wheels1").innerHTML=(`wheels is ${myHonda.wheels}`);
-document.getElementById("cylinders1").innerHTML=(`Engine Cylinders is ${myHonda.engine.cylinders}`);
-document.getElementById("size1").innerHTML=(`Engine Size is ${myHonda.engine.size}`);
+const card= document.getElementById("cardR")
 
-let Honda = {
+let bikes=[{
+  color: "red",
+  wheels: 2,
+  engine: {
+    cylinders: 4,
+    size: 2.2,
+  },},
+  {
     color: "blue",
     wheels: 2,
     engine: {
       cylinders: 4,
       size: 2.2,
     },
-}
-document.getElementById("color2").innerHTML=(`Color is ${Honda.color}`);
-document.getElementById("wheels2").innerHTML=(`wheels is ${Honda.wheels}`);
-document.getElementById("cylinders2").innerHTML=(`Engine Cylinders is ${Honda.engine.cylinders}`);
-document.getElementById("size2").innerHTML=(`Engine Size is ${Honda.engine.size}`);
-
-let Honda1 = {
-    color: "white",
-    wheels: 2,
-    engine: {
-      cylinders: 4,
-      size: 2.2,
-    },
-}
-document.getElementById("color3").innerHTML=(`Color is ${Honda1.color}`);
-document.getElementById("wheels3").innerHTML=(`wheels is ${Honda1.wheels}`);
-document.getElementById("cylinders3").innerHTML=(`Engine Cylinders is ${Honda1.engine.cylinders}`);
-document.getElementById("size3").innerHTML=(`Engine Size is ${Honda1.engine.size}`);
-
-let Honda2 = {
+},
+{
+  color: "white",
+  wheels: 2,
+  engine: {
+    cylinders: 4,
+    size: 2.2,
+  },
+},
+{
   color: "black",
   wheels: 2,
   engine: {
     cylinders: 4,
     size: 2.2,
   },
-}
-document.getElementById("color4").innerHTML=(`Color is ${Honda2.color}`);
-document.getElementById("wheels4").innerHTML=(`wheels is ${Honda2.wheels}`);
-document.getElementById("cylinders4").innerHTML=(`Engine Cylinders is ${Honda2.engine.cylinders}`);
-document.getElementById("size4").innerHTML=(`Engine Size is ${Honda2.engine.size}`);
-
-let Honda3 = {
+},
+{
   color: "lite blue",
   wheels: 2,
   engine: {
     cylinders: 4,
     size: 2.2,
   },
-}
-document.getElementById("color5").innerHTML=(`Color is ${Honda3.color}`);
-document.getElementById("wheels5").innerHTML=(`wheels is ${Honda3.wheels}`);
-document.getElementById("cylinders5").innerHTML=(`Engine Cylinders is ${Honda3.engine.cylinders}`);
-document.getElementById("size5").innerHTML=(`Engine Size is ${Honda3.engine.size}`);
-
-let Honda4 = {
-  color: "lite blue",
-  wheels: 2,
+},
+{
+  color: "green",
+  wheels: 4,
   engine: {
     cylinders: 4,
-    size: 2.2,
+    size: 1.2,
   },
-}
-document.getElementById("color6").innerHTML=(`Color is ${Honda4.color}`);
-document.getElementById("wheels6").innerHTML=(`wheels is ${Honda4.wheels}`);
-document.getElementById("cylinders6").innerHTML=(`Engine Cylinders is ${Honda4.engine.cylinders}`);
-document.getElementById("size6").innerHTML=(`Engine Size is ${Honda4.engine.size}`);
-
-let Honda5 = {
-  color: "lite blue",
-  wheels: 2,
+},
+{
+  color: "yellow",
+  wheels: 4,
   engine: {
     cylinders: 4,
-    size: 2.2,
+    size:1.5,
   },
-}
-document.getElementById("color7").innerHTML=(`Color is ${Honda5.color}`);
-document.getElementById("wheels7").innerHTML=(`wheels is ${Honda5.wheels}`);
-document.getElementById("cylinders7").innerHTML=(`Engine Cylinders is ${Honda5.engine.cylinders}`);
-document.getElementById("size7").innerHTML=(`Engine Size is ${Honda5.engine.size}`);
-
-let Honda6 = {
-  color: "lite blue",
-  wheels: 2,
+},
+{
+  color: "gold",
+  wheels: 4,
   engine: {
     cylinders: 4,
-    size: 2.2,
+    size: 1.2,
   },
-}
-document.getElementById("color8").innerHTML=(`Color is ${Honda6.color}`);
-document.getElementById("wheels8").innerHTML=(`wheels is ${Honda6.wheels}`);
-document.getElementById("cylinders8").innerHTML=(`Engine Cylinders is ${Honda6.engine.cylinders}`);
-document.getElementById("size8").innerHTML=(`Engine Size is ${Honda6.engine.size}`);
-
-let Honda7 = {
-  color: "lite blue",
-  wheels: 2,
+},
+{
+  color: "orange",
+  wheels: 4,
   engine: {
     cylinders: 4,
-    size: 2.2,
+    size: 1.2,
   },
-}
-document.getElementById("color9").innerHTML=(`Color is ${Honda7.color}`);
-document.getElementById("wheels9").innerHTML=(`wheels is ${Honda7.wheels}`);
-document.getElementById("cylinders9").innerHTML=(`Engine Cylinders is ${Honda7.engine.cylinders}`);
-document.getElementById("size9").innerHTML=(`Engine Size is ${Honda7.engine.size}`);
-
-let Honda8 = {
-  color: "lite blue",
-  wheels: 2,
+},
+{
+  color: "gray",
+  wheels: 4,
   engine: {
     cylinders: 4,
-    size: 2.2,
+    size: 1.5,
   },
-}
-document.getElementById("color10").innerHTML=(`Color is ${Honda8.color}`);
-document.getElementById("wheels10").innerHTML=(`wheels is ${Honda8.wheels}`);
-document.getElementById("cylinders10").innerHTML=(`Engine Cylinders is ${Honda8.engine.cylinders}`);
-document.getElementById("size10").innerHTML=(`Engine Size is ${Honda8.engine.size}`);
-
-let Honda9 = {
-  color: "lite blue",
-  wheels: 2,
+},
+{
+  color: "pearl",
+  wheels: 4,
   engine: {
     cylinders: 4,
-    size: 2.2,
+    size: 1.5,
   },
-}
-document.getElementById("color11").innerHTML=(`Color is ${Honda9.color}`);
-document.getElementById("wheels11").innerHTML=(`wheels is ${Honda9.wheels}`);
-document.getElementById("cylinders11").innerHTML=(`Engine Cylinders is ${Honda9.engine.cylinders}`);
-document.getElementById("size11").innerHTML=(`Engine Size is ${Honda9.engine.size}`);
-
-let Honda10 = {
-  color: "lite blue",
-  wheels: 2,
+},
+{
+  color: "red",
+  wheels: 4,
   engine: {
     cylinders: 4,
-    size: 2.2,
+    size: 1.5,
   },
-}
-document.getElementById("color12").innerHTML=(`Color is ${Honda10.color}`);
-document.getElementById("wheels12").innerHTML=(`wheels is ${Honda10.wheels}`);
-document.getElementById("cylinders12").innerHTML=(`Engine Cylinders is ${Honda10.engine.cylinders}`);
-document.getElementById("size12").innerHTML=(`Engine Size is ${Honda10.engine.size}`);
-
-let Honda11 = {
-  color: "lite blue",
-  wheels: 2,
+},
+{
+  color: "silver",
+  wheels: 4,
   engine: {
     cylinders: 4,
-    size: 2.2,
+    size: 1.2,
   },
-}
-document.getElementById("color13").innerHTML=(`Color is ${Honda11.color}`);
-document.getElementById("wheels13").innerHTML=(`wheels is ${Honda11.wheels}`);
-document.getElementById("cylinders13").innerHTML=(`Engine Cylinders is ${Honda11.engine.cylinders}`);
-document.getElementById("size13").innerHTML=(`Engine Size is ${Honda11.engine.size}`);
-
-let Honda12 = {
-  color: "lite blue",
-  wheels: 2,
+},
+{
+  color: "steel met",
+  wheels: 4,
   engine: {
     cylinders: 4,
-    size: 2.2,
+    size: 1.5,
   },
-}
-document.getElementById("color14").innerHTML=(`Color is ${Honda12.color}`);
-document.getElementById("wheels14").innerHTML=(`wheels is ${Honda12.wheels}`);
-document.getElementById("cylinders14").innerHTML=(`Engine Cylinders is ${Honda12.engine.cylinders}`);
-document.getElementById("size14").innerHTML=(`Engine Size is ${Honda12.engine.size}`);
-
-let Honda13 = {
-  color: "lite blue",
-  wheels: 2,
+},
+{
+  color: "black pearl",
+  wheels: 4,
   engine: {
     cylinders: 4,
-    size: 2.2,
+    size: 1.2,
   },
-}
-document.getElementById("color15").innerHTML=(`Color is ${Honda13.color}`);
-document.getElementById("wheels15").innerHTML=(`wheels is ${Honda13.wheels}`);
-document.getElementById("cylinders15").innerHTML=(`Engine Cylinders is ${Honda13.engine.cylinders}`);
-document.getElementById("size15").innerHTML=(`Engine Size is ${Honda13.engine.size}`);
-
-let Honda14 = {
-  color: "lite blue",
-  wheels: 2,
+},
+{
+  color: "hawk black",
+  wheels: 4,
   engine: {
     cylinders: 4,
-    size: 2.2,
+    size: 1.2,
   },
-}
-document.getElementById("color16").innerHTML=(`Color is ${Honda14.color}`);
-document.getElementById("wheels16").innerHTML=(`wheels is ${Honda14.wheels}`);
-document.getElementById("cylinders16").innerHTML=(`Engine Cylinders is ${Honda14.engine.cylinders}`);
-document.getElementById("size16").innerHTML=(`Engine Size is ${Honda14.engine.size}`);
-
-let Honda15 = {
-  color: "lite blue",
-  wheels: 2,
+},
+{
+  color: "urban titanium",
+  wheels: 4,
   engine: {
     cylinders: 4,
-    size: 2.2,
+    size: 1.5,
   },
-}
-document.getElementById("color17").innerHTML=(`Color is ${Honda15.color}`);
-document.getElementById("wheels17").innerHTML=(`wheels is ${Honda15.wheels}`);
-document.getElementById("cylinders17").innerHTML=(`Engine Cylinders is ${Honda15.engine.cylinders}`);
-document.getElementById("size17").innerHTML=(`Engine Size is ${Honda15.engine.size}`);
-
-let Honda16 = {
-  color: "lite blue",
-  wheels: 2,
+},
+{
+  color: "frosty white",
+  wheels: 4,
   engine: {
     cylinders: 4,
-    size: 2.2,
+    size: 1.5,
   },
-}
-document.getElementById("color18").innerHTML=(`Color is ${Honda16.color}`);
-document.getElementById("wheels18").innerHTML=(`wheels is ${Honda16.wheels}`);
-document.getElementById("cylinders18").innerHTML=(`Engine Cylinders is ${Honda16.engine.cylinders}`);
-document.getElementById("size18").innerHTML=(`Engine Size is ${Honda16.engine.size}`);
-
-let Honda17 = {
-  color: "lite blue",
-  wheels: 2,
+},
+{
+  color: "blue met",
+  wheels: 4,
   engine: {
     cylinders: 4,
-    size: 2.2,
+    size: 1.2,
   },
-}
-document.getElementById("color19").innerHTML=(`Color is ${Honda17.color}`);
-document.getElementById("wheels19").innerHTML=(`wheels is ${Honda17.wheels}`);
-document.getElementById("cylinders19").innerHTML=(`Engine Cylinders is ${Honda17.engine.cylinders}`);
-document.getElementById("size19").innerHTML=(`Engine Size is ${Honda17.engine.size}`);
-
-let Honda18 = {
-  color: "lite blue",
-  wheels: 2,
+},
+{
+  color: "gray met",
+  wheels: 4,
   engine: {
     cylinders: 4,
-    size: 2.2,
+    size: 1.2,
   },
-}
-document.getElementById("color20").innerHTML=(`Color is ${Honda18.color}`);
-document.getElementById("wheels20").innerHTML=(`wheels is ${Honda18.wheels}`);
-document.getElementById("cylinders20").innerHTML=(`Engine Cylinders is ${Honda18.engine.cylinders}`);
-document.getElementById("size20").innerHTML=(`Engine Size is ${Honda18.engine.size}`);
+},
+]
+ 
 
-let Honda19 = {
-  color: "lite blue",
-  wheels: 2,
-  engine: {
-    cylinders: 4,
-    size: 2.2,
-  },
-}
-document.getElementById("color21").innerHTML=(`Color is ${Honda19.color}`);
-document.getElementById("wheels21").innerHTML=(`wheels is ${Honda19.wheels}`);
-document.getElementById("cylinders21").innerHTML=(`Engine Cylinders is ${Honda19.engine.cylinders}`);
-document.getElementById("size21 ").innerHTML=(`Engine Size is ${Honda19.engine.size}`);
+bikes.forEach((element) => {
+  cardR.innerHTML +=
+  `<div class="col-4 mb-5">
+  <div class="card border-dark" style="width: 100%;">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item border-dark">
+        <h3>Color is ${element.color}</h3>
+      </li>
+      <li class="list-group-item border-dark">
+        <h3>wheels is ${element.wheels}</h3>
+      </li>
+      <li class="list-group-item border-dark">
+        <h3>Engine Cylinders is ${element.engine.cylinders}</h3>
+      </li>
+      <li class="list-group-item border-dark">
+        <h3>Engine Size is ${element.engine.size}</h3>
+      </li>
+    </ul>
+  </div>
+</div>`
+})
